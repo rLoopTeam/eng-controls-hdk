fd4079fe2d9c4f96bd860104c8590dc2bfadb459
Paul (mightyboat) <paullehenaff@msn.com>
2016-08-24 18:23:17 -0700

    Update all.ino
    Added code comments

--- HDK/all_hdk/all/all.ino
+++ HDK/all_hdk/all/all.ino
@@ -1,3 +1,7 @@
+/*
+ * HDK code
+ * Uses PID controller to stabilise the HDK
+ */
 #include <Servo.h>
 #include <Wire.h>
 //#include <Adafruit_MMA8451.h>
@@ -8,16 +12,19 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+// servo pins
 #define step4 3
 #define step3 5
 #define step2 4
 #define step1 6
 
+// hover engine pins
 #define pwmPin1 20
 #define pwmPin2 21
 #define pwmPin3 22
 #define pwmPin4 23
 
+// servo initialisation
 Servo myservo1;
 Servo myservo2; 
 Servo myservo3;
@@ -26,6 +33,7 @@ Servo myservo4;
 //Adafruit_MMA8451 mma = Adafruit_MMA8451();
 #define BNO055_SAMPLERATE_DELAY_MS (20)
 
+// bno055 orientation sensor
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 int pos1 = 0;
@@ -33,6 +41,7 @@ int pos2 = 0;
 int pos3 = 0;
 int pos4 = 0;
 
+// variables for PID controller
 static double input;
 static double last_error;
 static double point = 0;
@@ -46,12 +55,19 @@ double kd;
 bool direction;
 unsigned long lastTime = 0;
 
+/*
+ * Function for setting up the three PID constants
+ */
 void setup_constants(double p, double i, double d)
 {
    kp = p;
    ki = i;
    kd = d;
 }
+
+/*
+ * Function for starting the motors (hover engines)
+ */
 void start_motors()
 {
   analogWrite(pwmPin1, 249);
@@ -59,6 +75,9 @@ void start_motors()
   analogWrite(pwmPin3, 249);
   analogWrite(pwmPin4, 249);
 }
+/*
+ * Function for stopping the motors (hover engines)
+ */
 void stop_motors()
 {
   analogWrite(pwmPin1, 125);
@@ -67,6 +86,9 @@ void stop_motors()
   analogWrite(pwmPin4, 125);
 }
 
+/*
+ * Function for displaying the sensors details at initialisation
+ */
 void displaySensorDetails(void)
 {
   sensor_t sensor;
@@ -83,6 +105,9 @@ void displaySensorDetails(void)
   delay(500);
 }
 
+/*
+ * Function for displaying the status of the sensors
+ */
 void displaySensorStatus(void)
 {
   /* Get the system status values (mostly for debugging purposes) */
@@ -129,18 +154,27 @@ void displayCalStatus(void)
   Serial.print(mag, DEC);
 }
 
+/*
+ * Function for setting the servos including a calibration value
+ * Parameters match the hover engine/servo number on the HDK 
+ * e.g. 1 - servo 1, 2 - servo 2 etc
+ */
 void set_servo(double myservo1_val, double myservo2_val, double myservo3_val, double myservo4_val)
 {
+  // when setting the value of a servo, we include a correction value to account for hardware issues.
+  // the correction vlaue is simply the correction angle required to make the gimbal point straight down.
+  // if we set the servo to point to 90 degrees(striahgt down) but the gimbal points 5 degrees off, we have to add to remove
+  // 5 degrees in the write() function parameter as shown below.
   myservo1.write(myservo1_val - 0); // 90 - 45 == motor pointing downwards (when downwards is 90 degrees)
   myservo2.write(myservo2_val + 5);
   myservo3.write(myservo3_val - 5);
   myservo4.write(myservo4_val + 5);
 }
 
-
-
-
-
+/*
+ * Main setup function. 
+ * Reset the HDK, initialise sensors and PID and start motors
+ */
 void setup() {  
   pinMode(pwmPin1, OUTPUT);
   pinMode(pwmPin2, OUTPUT);
@@ -187,6 +221,12 @@ void setup() {
   setup_constants(0.2, 0, 0);
 
 }
+
+
+/*
+ * Main loop function.
+ * Run PID to adjust servos, to stabilise HDK
+ */
 void loop() {
 /* TTTTTTTTEEEEEEEEEEESSSSSSSSSSSSSTTTTTTTTTTTT
   for (pos1 = 1; pos1 < 90; pos1++)
@@ -204,17 +244,27 @@ void loop() {
 */  
   
   sensors_event_t event;
+  
+  // update sensor
   bno.getEvent(&event);
+  
   unsigned long now = millis();
   double timeChange = (double)(now - lastTime);
+  
+  // lagging variable for P error
   last_error = Err;
+  
+  // get orientation from sensor
   input = event.orientation.x;
+  
+  // calculate error from intended orientation (currently set to an absolute value of 0 (like a compass pointing north), but could be adjusted, or even made relative)
   Err = point - input;
 
-  if (input > 0 && input < 182){
+  // if the sensor is returning a value between 0 and 180, set reverse the error value and set directio to 0
+  if (input > 0 && input < 180){
     Err = -1*Err;
     direction = 0;
-  } else if ( input > 182) {
+  } else if ( input > 180) { // if sensor is on other side, modify the error
     Err = Err + 360;
     //Err = Err + 360;
     direction = 1;
@@ -226,21 +276,29 @@ void loop() {
 //    direction = 1;
 //  }
 //  else direction = 0;
-    
+  
+  // differnetial error calculation
   dErr = (Err - last_error)/timeChange;
   if (dErr < 0)
   {
     dErr = -dErr;
   }
   
+  // integral error calculation
   iErr = iErr + Err*timeChange;
   if (iErr < 0)
   {
     iErr = -iErr;
   }
+  
+  // final PID value
   voltage = kp * Err + ki * iErr + kd * dErr;
+  
+  // cap the voltage value to 20 because any more and the gimbal will hit the ground or the top plate
   if (voltage > 20)
     voltage = 20;
+  
+  // set servo values according to the direction and apply the PID value
   if (direction)
   {
     //myservo2.write(90);
@@ -249,7 +307,7 @@ void loop() {
     //myservo4.write(90 + voltage);
     //set_servo(90 + voltage, 90, 90, 100 - voltage);
     //set_servo(90 + voltage, 90 + voltage, 90 - voltage, 100 - voltage);
-    set_servo(90, 90 - voltage, 90, 90 + voltage);
+    set_servo(90, 90 - voltage, 90, 90 + voltage); // set two diagonal motors to always point downwards, and the other two to point according to the PID
   }
   else if (!direction)
   {
@@ -259,9 +317,11 @@ void loop() {
     //myservo3.write(90 - voltage);
     //set_servo(90, 95 - voltage, 90 + voltage, 90); 
     //set_servo(90 - voltage, 95 - voltage, 90 + voltage, 90 + voltage); 
-    set_servo(90 + voltage, 90, 90 - voltage, 90);
+    set_servo(90 + voltage, 90, 90 - voltage, 90); // set two diagonal motors to always point downwards, and the other two to point according to the PID
   }
-  lastTime = now;
+  
+  // used for time calculation
+  lastTime = now; 
     
     /* Display the floating point data */
   Serial.print("X: ");
